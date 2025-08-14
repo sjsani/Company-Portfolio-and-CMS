d38d8bc7780597ff7393071613d3e8f10b1abb67
sjsani <sjsani.official@gmail.com>
2025-08-14 15:23:54 +0500

    sixth commit

--- nova-website/lib/sanity.ts
+++ nova-website/lib/sanity.ts
@@ -1,9 +1,8 @@
-// lib/sanity.ts
 import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: "wkexqpkp", // Replace this
-  dataset: "production",
-  apiVersion: "2023-01-01",
-  useCdn: false,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  useCdn: true,
 });
