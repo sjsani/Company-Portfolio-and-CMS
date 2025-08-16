ce85ae73c3eca087917fa96b90b5409a4bd7a8a2
sjsani <sjsani.official@gmail.com>
2025-08-16 12:17:22 +0500

    final commit last commit

--- nova-website/src/app/layout.tsx
+++ nova-website/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { siteSettingsQuery,heroQuery } from "../../lib/queries";
+import { sanityClient } from "../../lib/sanity";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,23 +14,31 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Nova Digital",
-  
-};
+// This replaces your static metadata export
+export async function generateMetadata(): Promise<Metadata> {
+  const site = await sanityClient.fetch(siteSettingsQuery);
+  const hero = await sanityClient.fetch(heroQuery);
+
+  return {
+    title: site?.siteTitle || "Nova Website",
+    description: hero?.heroDescription || "Welcome to Nova Digital",
+    icons: {
+      icon: site.logoUrl|| "/favicon.ico", // fallback if Sanity doesn't have favicon
+    },
+  };
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
   );
 }
+
